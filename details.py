942140e90d78983f2ed1141a726f03bb5075cf72
arzaan <xdarzanplug@gmail.com>
2024-02-04 15:40:52 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,4 +1,4 @@
 api_id =  4942197
 api_hash = "13248a2c551b73193969b42194023635"
-bot_token = "6520039490:AAHc0eq1k5G1AWX_x-Hc4Uq_lYRxYbZQubI"
+bot_token = "6844922552:AAEeXWgjAUPZr_Y-pu1k3KcLm4Jil4yvmWU"
 
